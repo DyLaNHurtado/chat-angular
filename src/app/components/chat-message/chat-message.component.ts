ea6e0bb435b31ea5f6d2d218624a7135122563c0
DyLaNHurtado <dylanhurtado43@gmail.com>
2022-03-28 17:38:20 +0200

    adding message component

--- src/app/components/chat-message/chat-message.component.ts
+++ src/app/components/chat-message/chat-message.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ChatMessageComponent implements OnInit {
 
   constructor() { }
-
+  
   ngOnInit() {
   }
 
--- src/app/components/chat-message/chat-message.module.ts
+++ src/app/components/chat-message/chat-message.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChatMessageComponent } from './chat-message.component';
 import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   imports: [
     CommonModule,
+    FlexLayoutModule,
     MatCardModule,
 
   ],
