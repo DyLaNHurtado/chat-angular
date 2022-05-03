5d16a26a54a49fe425d17c345fbdaf95c8313025
DyLaNHurtado <dylanhurtado43@gmail.com>
2022-05-03 18:03:33 +0200

    Intentando que se muestre los avatares en el Contact List

--- src/app/components/chat-box/chat-box.component.ts
+++ src/app/components/chat-box/chat-box.component.ts
@@ -1,7 +1,17 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import { Component, Inject, Input, OnInit, SecurityContext } from '@angular/core';
+import {
+  Component,
+  Inject,
+  Input,
+  OnInit,
+  SecurityContext,
+} from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import {
+  MatDialog,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
 import { DomSanitizer } from '@angular/platform-browser';
 import { CookieService } from 'ngx-cookie-service';
 import { HttpclientService } from 'src/app/httpclient.service';
@@ -9,116 +19,124 @@ import { HttpclientService } from 'src/app/httpclient.service';
 @Component({
   selector: 'app-chat-box',
   templateUrl: './chat-box.component.html',
-  styleUrls: ['./chat-box.component.scss']
+  styleUrls: ['./chat-box.component.scss'],
 })
 export class ChatBoxComponent implements OnInit {
-  @Input() contact:any;
+  @Input() contact: any;
   public textArea: string = '';
   public isEmojiPickerVisible: boolean;
-  public themeDark:boolean;
+  public themeDark: boolean;
   public entityForm: FormGroup;
   public input: string;
-  static base64data:any;
-  avatar:string="https://raw.githubusercontent.com/DyLaNHurtado/chat-angular/develop/src/assets/img/loading-gif.gif";
-  constructor(public dialog: MatDialog,private cookieService:CookieService,public httpService:HttpclientService,private _sanitizer: DomSanitizer) { }
+  static base64data: any;
+  avatar: string =
+    'https://raw.githubusercontent.com/DyLaNHurtado/chat-angular/develop/src/assets/img/loading-gif.gif';
+  constructor(
+    public dialog: MatDialog,
+    private cookieService: CookieService,
+    public httpService: HttpclientService,
+    private _sanitizer: DomSanitizer
+  ) {}
 
   ngOnInit() {
     this.setTheme();
     this.entityForm = new FormGroup({
-      input: new FormControl("",[]),
+      input: new FormControl('', []),
     });
     this.getImageApi();
   }
 
-  public base64dataToImage() : void {
-    setTimeout(()=>{
-      if(!ChatBoxComponent.base64data){
-        console.log("nooo");
-        this.getImageApi()
-      }else{
-        console.log("siiii");
-        
-        this.avatar = this._sanitizer.sanitize(SecurityContext.RESOURCE_URL,this._sanitizer.bypassSecurityTrustResourceUrl(ChatBoxComponent.base64data));
-      }
-    },1) 
-}
+  public base64dataToImage(): void {
+    if (!ChatBoxComponent.base64data) {
+      console.log('nooo');
+      this.getImageApi();
+    } else {
+      console.log('siiii');
 
-private getImageApi(){
-  if(!this.contact.avatar.includes("https://ui-avatars.com/api/")){
-      this.httpService.getAvatar(this.contact.avatar.replace("uploads/",""))
-        .subscribe(res => {
-         if(res.status == 200){
-           var reader = new FileReader();
-           reader.readAsDataURL(res.body);
-             reader.onload = () => {
-               ChatBoxComponent.base64data = reader.result;
-             }
-             setTimeout(()=>{
-               this.base64dataToImage();
-               console.log(res.body);
-             },1000)
-         }
-         },
-         (errorRes:HttpErrorResponse) => {
-           console.error(errorRes);
-         });
-        }else{
-          this.avatar=this.contact.avatar;}
-}
-
-  
-  private setTheme(){
-    if(JSON.parse(localStorage.getItem('theme'))==1){
-      this.themeDark=true;
-    }else{
-      this.themeDark=false;
+      this.avatar = this._sanitizer.sanitize(
+        SecurityContext.RESOURCE_URL,
+        this._sanitizer.bypassSecurityTrustResourceUrl(
+          ChatBoxComponent.base64data
+        )
+      );
     }
   }
-  public getBg(){
+
+  private getImageApi() {
+    if (!this.contact.avatar.includes('https://ui-avatars.com/api/')) {
+      this.httpService
+        .getAvatar(this.contact.avatar.replace('uploads/', ''))
+        .subscribe(
+          (res) => {
+            if (res.status == 200) {
+              var reader = new FileReader();
+              reader.readAsDataURL(res.body);
+              reader.onload = () => {
+                ChatBoxComponent.base64data = reader.result;
+                document.dispatchEvent(new Event('avatarReadedChatBox'));
+              };
+              document.addEventListener('avatarReadedChatBox', () => {
+                this.base64dataToImage();
+                console.log(res.body);
+              });
+            }
+          },
+          (errorRes: HttpErrorResponse) => {
+            console.error(errorRes);
+          }
+        );
+    } else {
+      this.avatar = this.contact.avatar;
+    }
+  }
+
+  private setTheme() {
+    if (JSON.parse(localStorage.getItem('theme')) == 1) {
+      this.themeDark = true;
+    } else {
+      this.themeDark = false;
+    }
+  }
+  public getBg() {
     return JSON.parse(localStorage.getItem('bg'));
   }
-  public getColor(){
+  public getColor() {
     return JSON.parse(localStorage.getItem('bg_color'));
   }
   public addEmoji(event) {
     this.textArea = `${this.textArea}${event.emoji.native}`;
- }
- 
- public sendMessage(){
-  this.input=this.entityForm.get("input").value;
-  if(this.input.trim()!=""){
-    alert(this.input.trim());
-    this.entityForm.get("input").setValue("");
-    this.isEmojiPickerVisible=false
-   }
   }
 
-  public openImageDialog(){
+  public sendMessage() {
+    this.input = this.entityForm.get('input').value;
+    if (this.input.trim() != '') {
+      alert(this.input.trim());
+      this.entityForm.get('input').setValue('');
+      this.isEmojiPickerVisible = false;
+    }
+  }
+
+  public openImageDialog() {
     console.log(this.avatar);
-    
-    this.dialog.open(ImageDialog,{
-      data:{img:this.avatar}
+
+    this.dialog.open(ImageDialog, {
+      data: { img: this.avatar },
     });
   }
-  
-
 }
-
 
 @Component({
   selector: 'image-dialog',
   templateUrl: 'image.dialog.html',
 })
 export class ImageDialog {
-  image:string;
-  ngOnInit(){
-    this.image=this.data.img;
+  image: string;
+  ngOnInit() {
+    this.image = this.data.img;
     console.log(this.data.img);
-    
   }
   constructor(
-    @Inject(MAT_DIALOG_DATA) private data:any,
-    private dialogRef:MatDialogRef<ImageDialog>
-  ){
-  }
+    @Inject(MAT_DIALOG_DATA) private data: any,
+    private dialogRef: MatDialogRef<ImageDialog>
+  ) {}
 }
--- src/app/components/contact-list/contact-list.component.ts
+++ src/app/components/contact-list/contact-list.component.ts
@@ -1,155 +1,163 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import { Component, EventEmitter, OnInit, Output, SecurityContext, ViewChild } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  OnInit,
+  Output,
+  SecurityContext,
+  ViewChild,
+} from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSelectionList } from '@angular/material/list';
 import { DomSanitizer } from '@angular/platform-browser';
 import { CookieService } from 'ngx-cookie-service';
-import { Observable, Subject } from 'rxjs';
-import {first, map, startWith, take, takeUntil} from 'rxjs/operators';
+import { Observable, of, Subject } from 'rxjs';
+import {
+  concatMap,
+  delay,
+  first,
+  map,
+  startWith,
+  take,
+  takeUntil,
+} from 'rxjs/operators';
 import { HttpclientService } from 'src/app/httpclient.service';
 import { AddDialogComponent } from '../add-dialog/add-dialog.component';
 
 @Component({
   selector: 'app-contact-list',
   templateUrl: './contact-list.component.html',
-  styleUrls: ['./contact-list.component.scss']
+  styleUrls: ['./contact-list.component.scss'],
 })
 export class ContactListComponent implements OnInit {
+  @Output() selectContact = new EventEmitter();
+  @ViewChild('contact') contact: MatSelectionList;
+  contactList: string[] = [];
+  filteredOptions: Observable<string[]>;
+  myControl: FormControl;
+  themeDark: boolean;
+  user: any;
+  contactObjectsList = [];
+  static base64data: any;
+  index: number;
+  avatar: string;
+  avatarList: string[] = [];
 
-  @Output() selectContact= new EventEmitter();
-  @ViewChild("contact") contact:MatSelectionList;
-  contactList:string[]=[]
-  filteredOptions:Observable<string[]>;
-  myControl:FormControl;
-  themeDark:boolean;
-  user:any;
-  contactObjectsList=[];
-  static base64data:any;
-  index:number;
-  avatar:string;
-
-  constructor(public dialog: MatDialog,private cookieService:CookieService,public httpService:HttpclientService,private _sanitizer: DomSanitizer) {
+  constructor(
+    public dialog: MatDialog,
+    private cookieService: CookieService,
+    public httpService: HttpclientService,
+    private _sanitizer: DomSanitizer
+  ) {
     this.myControl = new FormControl();
   }
 
   ngOnInit() {
-    
     this.setTheme();
     this.myControl = new FormControl();
     this.setContactList();
-    setTimeout(()=>{
+  }
+
+  public base64dataToImage(): string {
+    return this._sanitizer.sanitize(
+      SecurityContext.RESOURCE_URL,
+      this._sanitizer.bypassSecurityTrustResourceUrl(
+        ContactListComponent.base64data
+      )
+    );
+  }
+
+  public getImageApi(avatar: string) {
+    if (!avatar.includes('https://ui-avatars.com/api/')) {
+      this.httpService.getAvatar(avatar.replace('uploads/', '')).subscribe(
+        (res) => {
+          if (res.status == 200) {
+            var reader = new FileReader();
+            reader.readAsDataURL(res.body);
+            reader.onload = () => {
+              ContactListComponent.base64data = reader.result;
+              document.dispatchEvent(new Event('avatarReadedCL'));
+            };
+            document.addEventListener('avatarReadedCL', () => {
+              return of(this.base64dataToImage());
+              console.log(res.body);
+            });
+          }
+        },
+        (errorRes: HttpErrorResponse) => {
+          console.error(errorRes);
+        }
+      );
+    } else {
+      return of(this.avatar);
+    }
+  }
+
+  private setContactList() {
+    this.httpService
+      .getFullUser(JSON.parse(this.cookieService.get('payload')).id)
+      .subscribe(
+        (res) => {
+          if (res.status == 200) {
+            this.user = res.body;
+            this.contactList = this.user.contacts.map((user) => {
+              return user.name;
+            });
+            this.contactObjectsList = this.user.contacts;
+            this.avatarList = this.user.contacts.map(
+              (contact) => contact.avatar
+            );
+            console.log(this.avatarList);
+
+            document.dispatchEvent(new Event('gotUsersCL'));
+          }
+        },
+        (errorRes: HttpErrorResponse) => {
+          console.error(errorRes);
+        }
+      );
+    document.addEventListener('gotUsersCL', () => {
       this.filteredOptions = this.myControl.valueChanges.pipe(
         startWith(''),
-        map(value => this._filter(value)),
+        map((value) => this._filter(value))
       );
-    },700);
+    });
   }
 
-  public base64dataToImage() : void {
-    setTimeout(()=>{
-      if(!ContactListComponent.base64data){
-        console.log("nooo");
-        this.getImageApi(this.index);
-      }else{
-        console.log("siiii");
-        this.avatar = this._sanitizer.sanitize(SecurityContext.RESOURCE_URL,this._sanitizer.bypassSecurityTrustResourceUrl(ContactListComponent.base64data));
-      }
-    },1)
-    
-}
-
-  private setContactList(){
-    this.httpService.getFullUser(JSON.parse(this.cookieService.get('payload')).id)
-      .subscribe(res => {
-        if(res.status==200){
-          this.user=res.body;
-          this.contactList = this.user.contacts.map((user)=>{return user.name;})
-          this.contactObjectsList=this.user.contacts;
-          
-        }
-      },
-        (errorRes:HttpErrorResponse) => {
-          console.error(errorRes);
-        });
-          
-    }
-  
-  private setTheme(){
-    if(JSON.parse(localStorage.getItem('theme'))==1){
-      this.themeDark=true;
-    }else{
-      this.themeDark=false;
+  private setTheme() {
+    if (JSON.parse(localStorage.getItem('theme')) == 1) {
+      this.themeDark = true;
+    } else {
+      this.themeDark = false;
     }
   }
-  public getContactAvatar(name:string){
-    if(name.trim()!=""){
+
+  public getIndexByName(name: string) {
     let position = 0;
-    for (let i = 0 ;i < this.contactList.length ; i++){
-      if(this.contactList[i]==name){
-        position=i;
+    for (let i = 0; i < this.contactList.length; i++) {
+      if (this.contactList[i] == name) {
+        position = i;
       }
-    }
-    this.index=position;
-    return this.getImageApi(position);
-  }else{
-    return "";
-  }
-    }
-
-    private getIndexByName(name:string){
-      let position = 0;
-      for (let i = 0 ;i < this.contactList.length ; i++){
-        if(this.contactList[i]==name){
-          position=i;
-        }
     }
     return position;
   }
 
-    private getImageApi(index){
-      if(!this.contactObjectsList[index].avatar.includes("https://ui-avatars.com/api/")){
-        setTimeout(()=>{
-        this.httpService.getAvatar(this.contactObjectsList[index].avatar.replace("uploads/",""))
-          .subscribe(res => {
-           if(res.status == 200 || res.status == 304){
-             var reader = new FileReader();
-             reader.readAsDataURL(res.body);
-                reader.onload = () => {
-                  ContactListComponent.base64data = reader.result;
-                  setTimeout(()=>{
-                  this.base64dataToImage();
-                  console.log(this.avatar);
-                  
-                  return this.avatar;},2000);
-                }
-            }else{return this.avatar;}
-                
-            return this.avatar;
-           },
-           (errorRes:HttpErrorResponse) => {
-             console.error(errorRes);
-           });},5000);
-          }else{  
-            return this.contactObjectsList[index].avatar;
-          }
-            
-    }
-    
-
   private _filter(value: string): string[] {
     const filterValue = value.toLowerCase();
-    return this.contactList.filter(option => option.toLowerCase().includes(filterValue));
+    return this.contactList.filter((option) =>
+      option.toLowerCase().includes(filterValue)
+    );
   }
-  public sendName(){
-    this.selectContact.emit(this.contactObjectsList[this.getIndexByName(this.contact.selectedOptions.selected[0].value)]);
-    console.log(this.contact.selectedOptions.selected[0].value, this.contact.selectedOptions.selected[0].value);
-    
+  public sendName() {
+    this.selectContact.emit(
+      this.contactObjectsList[
+        this.getIndexByName(this.contact.selectedOptions.selected[0].value)
+      ]
+    );
   }
 
-  public openAddDialog(){
+  public openAddDialog() {
     this.dialog.open(AddDialogComponent);
   }
-
 }
-
