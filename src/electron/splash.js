a6bb91321d185a45be92bf1a570314cee5844f34
Dylan Hurtado López <dylahurtado43@gmail.com>
2022-05-14 23:45:14 +0200

    Close native dialog in electron app

--- src/electron/splash.js
+++ src/electron/splash.js
@@ -1,7 +1,7 @@
 const { BrowserWindow } = require("electron");
 const path = require('path');
 const url = require('url');
-var splash = new BrowserWindow({
+let splash = new BrowserWindow({
     width: 500, 
     height: 300, 
     frame: false,
