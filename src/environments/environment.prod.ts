1e89a8e6f80293ce4ece8b4c45567647d395df76
DyLaNHurtado <dylanhurtado43@gmail.com>
2022-06-01 15:18:44 +0200

    Adding backend cloud urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  serverSocket: 'http://localhost:5000',
-  APIUri: 'http://localhost:8888/api/v1/',
+  serverSocket: 'http://localhost:5000' || 'http://tcp-mo1.mogenius.io:18799',
+  APIUri: 'http://localhost:8888/api/v1/'|| 'http://tcp-mo1.mogenius.io:31259/api/v1/',
   wsEndpoint: 'ws://localhost:8081/',
   RTCPeerConfiguration: {
     iceServers: [
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  serverSocket: 'http://localhost:5000',
-  APIUri: 'http://localhost:8888/api/v1/',
+  serverSocket: 'http://localhost:5000' || 'http://tcp-mo1.mogenius.io:18799',
+  APIUri: 'http://localhost:8888/api/v1/'|| 'http://tcp-mo1.mogenius.io:31259/api/v1/',
   wsEndpoint: 'ws://localhost:8081/',
   RTCPeerConfiguration: {
     iceServers: [
