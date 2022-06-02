a30400fef2e5c73b99c9fcbb0dcc4da25e3cd7b4
DyLaNHurtado <dylanhurtado43@gmail.com>
2022-06-02 12:58:02 +0200

    Fixing error with cors in netlify

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   serverSocketLocal: 'http://localhost:5000',
-  serverSocketCloud: 'chat-api-node-prod-cosmos-chat-bu1tlu.mo1.mogenius.io:80',
+  serverSocketCloud: 'https://chat-api-node-prod-cosmos-chat-bu1tlu.mo1.mogenius.io/',
   APILocalUri: 'http://localhost:8888/api/v1/',
   APICloudUri: 'http://tcp-mo1.mogenius.io:31259/api/v1/',
   APIOption: 'CLOUD',
