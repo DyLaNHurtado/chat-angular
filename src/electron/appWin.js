6d75652b735cfaf183366577a5fb80e06bb89752
Dylan Hurtado López <dylahurtado43@gmail.com>
2022-06-10 22:36:05 +0200

    Deleting devTools in electron app

--- src/electron/appWin.js
+++ src/electron/appWin.js
@@ -1,43 +1,41 @@
-const { BrowserWindow,dialog } = require("electron");
+const { BrowserWindow, dialog } = require("electron");
 let appWin = new BrowserWindow({
-    width: 1200,
-    height: 800,
-    minWidth:710,
-    minHeight:710,
-    title: "Cosmos Desktop",
-    show:false,
-    resizable: true,
-    icon: "https://raw.githubusercontent.com/DyLaNHurtado/chat-angular/develop/src/assets/img/logo.png",
-    webPreferences: {
-        contextIsolation: false,
-        nodeIntegration: true,
-    }
+  width: 1200,
+  height: 800,
+  minWidth: 710,
+  minHeight: 710,
+  title: "Cosmos Desktop",
+  show: false,
+  resizable: true,
+  icon: "https://raw.githubusercontent.com/DyLaNHurtado/chat-angular/develop/src/assets/img/logo.png",
+  webPreferences: {
+    contextIsolation: false,
+    nodeIntegration: true,
+  },
 });
 appWin.loadURL(`https://cosmos-chat.netlify.app`);
 appWin.center();
 appWin.setMenu(null);
 
-    appWin.webContents.openDevTools();
+appWin.on("close", (event) => {
+  const resDialog = dialog.showMessageBoxSync(appWin, {
+    message: " Do you want to leave Cosmos? ",
+    buttons: ["Yes", "No"],
+  });
+  if (resDialog === 1) {
+    event.preventDefault();
+  }
+});
 
-    appWin.on('close', (event) =>{
-        const resDialog = dialog.showMessageBoxSync(appWin,{
-            message : ' Do you want to leave Cosmos? ',
-            buttons:['Yes','No']
-        });
-        if(resDialog === 1){
-           event.preventDefault();
-        }
-    });
+appWin.on("ready-to-show", () => {
+  const splash = require("./splash");
+  setTimeout(() => {
+    if (!splash.isDestroyed()) {
+      splash.close();
+    }
+    appWin.show();
+    appWin.focus();
+  }, 5000);
+});
 
-    appWin.on('ready-to-show',()=>{
-        const splash = require('./splash');
-        setTimeout(()=>{
-            if(!splash.isDestroyed()){
-                splash.close();
-            }
-            appWin.show();
-            appWin.focus();
-        },5000);
-    });
-
-module.exports=appWin;
+module.exports = appWin;
