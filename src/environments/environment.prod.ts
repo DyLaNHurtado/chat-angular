8b88b8d568a0cb5825cc5b3c51918a44daf40d40
DyLaNHurtado <dylanhurtado43@gmail.com>
2022-11-17 18:20:45 +0100

    updating cloud ref

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   serverSocketLocal: 'http://localhost:5000',
   serverSocketCloud: 'https://chat-api-node-prod-cosmos-chat-bu1tlu.mo1.mogenius.io/',
   APILocalUri: 'http://localhost:8888/api/v1/',
-  APICloudUri: 'http://tcp-mo1.mogenius.io:31259/api/v1/',
+  APICloudUri: 'https://cosmos-chat-api.onrender.com/api/v1/',
   APIOption: 'CLOUD',
   wsEndpoint: 'ws://localhost:8081/',
   RTCPeerConfiguration: {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,9 +5,9 @@
 export const environment = {
   production: false,
   serverSocketLocal: 'http://localhost:5000',
-  serverSocketCloud: 'https://chat-api-node-prod-cosmos-chat-bu1tlu.mo1.mogenius.io/',
+  serverSocketCloud: 'https://cosmos-chat-api.onrender.com:5000',
   APILocalUri: 'http://localhost:8888/api/v1/',
-  APICloudUri: 'http://tcp-mo1.mogenius.io:31259/api/v1/',
+  APICloudUri: 'https://cosmos-chat-api.onrender.com/api/v1/',
   APIOption: 'CLOUD',
   wsEndpoint: 'ws://localhost:8081/',
   RTCPeerConfiguration: {
